f709b78f61b0564740171a5000b813e183adb536
9346833313 <114649048+9346833313@users.noreply.github.com>
2022-10-16 14:42:14 +0530

    project 1 files added
    it the first project in our institute.

---
+++ test.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+var stu =[0,1,2,3,4];
+console.log(stu[3]);
+function anitha(val){
+    let name ='anith;'
+   if(val ==true){
+
+        console.log(name)
+      }
+   }
+
+}
+anitha(true);
